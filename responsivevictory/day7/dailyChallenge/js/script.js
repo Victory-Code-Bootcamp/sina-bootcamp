a82e60a06df77b52b50cef3b5e5d11bb103b5e64
4masina <montmom143@gmail.com>
2024-07-16 20:26:11 -0700

    Day 7 challenge

---
+++ responsivevictory/day7/dailyChallenge/js/script.js
@@ -0,0 +1,18 @@
+var myFirstName = "masina";
+
+console.log(myFirstName.length);
+
+var luckyNumber1 = 8;
+var luckyNumber2 = 23;
+
+const name = "masina";
+const message = "hello, ${name}!";
+console.log('My first lucky number is ${luckyNumber1} my second lucky number is ${luckyNumber2}');
+
+let luckyNumberOne = 8; // Replace with your first lucky number
+let luckyNumberTwo = 7; // Replace with your second lucky number
+
+console.log(`My first lucky number is ${luckyNumberOne} and my second favorite lucky number is ${luckyNumberTwo}`);
+
+
+
